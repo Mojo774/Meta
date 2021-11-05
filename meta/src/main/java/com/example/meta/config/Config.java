0fd61e51589707124693bbdd6a3c4233154b2a76
Aleksandr Kartashov <aleksandrmon1@gmail.com>
2021-11-05 14:42:42 +0300

    added timeout

--- meta/src/main/java/com/example/meta/config/Config.java
+++ meta/src/main/java/com/example/meta/config/Config.java
@@ -1,16 +1,23 @@
 package com.example.meta.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class Config {
-
+    //todo вынести время в пропиртиес
     @Bean
     @LoadBalanced
-    public RestTemplate getRestTemplate(){
-        return new RestTemplate();
+    public RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+
+        return  restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
     }
 }
